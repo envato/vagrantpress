4178c6f84adaa4c55ebcea3b0fa711df49d76c69
Pete Johns <pete.johns@envato.com>
2014-06-03 05:45:25 +1000

    wordpress depends upon mysql

--- puppet/modules/wordpress/manifests/init.pp
+++ puppet/modules/wordpress/manifests/init.pp
@@ -2,15 +2,19 @@
 
 class wordpress {
 
+  require mysql
+
   # Create the Wordpress database
   exec { 'create-database':
     unless  => '/usr/bin/mysql -u root -pvagrant wordpress',
     command => '/usr/bin/mysql -u root -pvagrant --execute="create database wordpress"',
+    require => Class['mysql'],
   }
 
   exec { 'create-user':
     unless  => '/usr/bin/mysql -u wordpress -pwordpress wordpress',
     command => '/usr/bin/mysql -u root -pvagrant --execute="GRANT ALL PRIVILEGES ON wordpress.* TO \'wordpress\'@\'localhost\' IDENTIFIED BY \'wordpress\'"',
+    require => [Class['mysql'], Exec['create-database'], ],
   }
 
   # Get a new copy of the latest wordpress release
@@ -36,26 +40,31 @@ class wordpress {
   exec { 'load-db':
     command => '/usr/bin/mysql -u wordpress -pwordpress wordpress < /tmp/wordpress-db.sql && touch /home/vagrant/db-created',
     creates => '/home/vagrant/db-created',
+    require => Class['mysql'],
   }
 
   # Copy a working wp-config.php file for the vagrant setup.
   file { '/vagrant/wordpress/wp-config.php':
-    source => 'puppet:///modules/wordpress/wp-config.php'
+    source => 'puppet:///modules/wordpress/wp-config.php',
+    require => Exec['untar-wordpress'],
   }
 
   # Create the Wordpress Unit Tests database
   exec { 'create-tests-database':
     unless  => '/usr/bin/mysql -u root -pvagrant wp_tests',
     command => '/usr/bin/mysql -u root -pvagrant --execute="create database wp_tests"',
+    require => Class['mysql'],
   }
 
   exec { 'create-tests-user':
     unless  => '/usr/bin/mysql -u wordpress -pwordpress',
     command => '/usr/bin/mysql -u root -pvagrant --execute="GRANT ALL PRIVILEGES ON wp_tests.* TO \'wordpress\'@\'localhost\' IDENTIFIED BY \'wordpress\'"',
+    require => [Class['mysql'], Exec['create-tests-database'], ],
   }
 
   # Copy a working wp-tests-config.php file for the vagrant setup.
   file { '/vagrant/wordpress/wp-tests-config.php':
-    source => 'puppet:///modules/wordpress/wp-tests-config.php'
+    source  => 'puppet:///modules/wordpress/wp-tests-config.php',
+    require => Exec['untar-wordpress'],
   }
 }
