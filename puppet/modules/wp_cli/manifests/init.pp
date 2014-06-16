18d2976fac34c0b2bb4a29e5d38c1d82ee4b9f60
John Viner <johnpviner@gmail.com>
2014-06-17 03:13:01 +1000

    added the wget

--- puppet/modules/wp_cli/manifests/init.pp
+++ puppet/modules/wp_cli/manifests/init.pp
@@ -2,6 +2,12 @@
 
 class wp_cli {
 
+  exec { "wp_cli_source":
+    command => "/usr/bin/wget https://github.com/wp-cli/builds/blob/gh-pages/deb/php-wpcli_0.15.0_all.deb",
+    cwd => "/tmp/",
+    creates => "/tmp/php-wpcli_0.15.0_all.deb",
+    before => Package['wp_cli']
+  }
 
   package { 'wp_cli':
     require  => File['/tmp/php-wpcli_0.15.0_all.deb'],
